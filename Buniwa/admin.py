a3b9238ab0a2fd2f8b658e9467e1c05e524830f0
Charles Muema <79078172+maundulaurent@users.noreply.github.com>
2024-06-22 15:27:32 +0300

    Delete Buniwa directory

--- Buniwa/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import theBlog
-from .models import PortfolioPost
-from.models import theTeam
-
-admin.site.register(theBlog)
-admin.site.register(PortfolioPost)
-admin.site.register(theTeam)
--- Buniwa/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BuniwaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Buniwa'
